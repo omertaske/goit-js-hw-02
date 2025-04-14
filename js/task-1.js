969385112f6e5a930a2b6a369aa80552416e6f26
Ömer Taşkesen <58364159+omertaske@users.noreply.github.com>
2025-04-14 10:31:50 +0300

    second commit

--- js/task-1.js
+++ js/task-1.js
@@ -9,9 +9,13 @@ let total = quantity*pricePerDroid;
 if (total > customerCredits) {
     return "Insufficient funds!";
 } else {
-   return "You ordered <quantity> droids worth <totalPrice> credits!";
+   return "You ordered " + quantity + " droids worth " + total +  " credits!";
 }
 
 }
 
 console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+function checkForSpam(message) {
+    // Metni küçük harfe çeviriyoruz
+    const lowerCaseMessage = message.toLowerCase();
+  
+    // "spam" veya "sale" kelimelerinden biri geçiyorsa true döndür
+    return !!(lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale"));
+  }
+
+
+
+  
+  console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+  
+  
